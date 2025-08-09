008777d983461e263830cc177be14a6f41af0684
Wahyudi-1 <147951968+Wahyudi-1@users.noreply.github.com>
2025-08-10 06:46:02 +0700

    Update config.js

--- config.js
+++ config.js
@@ -5,4 +5,4 @@ const SUPABASE_URL = 'https://rdpkbvbluvrvvoiuxxai.supabase.co'; // <- Ganti ini
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJkcGtidmJsdXZydnZvaXV4eGFpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTQ3NzgyOTQsImV4cCI6MjA3MDM1NDI5NH0.78BNbh4M7bSavpFmdZR9KBPzflvCm912xa_12v2-wFY'; // <- Ganti ini
 
 // Inisialisasi Supabase Client
-const supabase = supabase.createClient('https://rdpkbvbluvrvvoiuxxai.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJkcGtidmJsdXZydnZvaXV4eGFpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTQ3NzgyOTQsImV4cCI6MjA3MDM1NDI5NH0.78BNbh4M7bSavpFmdZR9KBPzflvCm912xa_12v2-wFY');
+const supabase = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
